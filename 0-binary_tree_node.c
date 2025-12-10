09e6731203cf50f5793059f0a28baccb5587653c
poloelo <p.gioria2304@gmail.com>
2025-12-10 15:38:57 +0100

    rectif

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,6 +8,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
     new_node->parent = parent;
     new_node->n = value;
+    new_node->left = NULL;
+    new_node->left = NULL;
     
     return new_node;
 }
+
