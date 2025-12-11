e62e6f834334813cefe6a610fb1674e8bc4bdbf7
poloelo <p.gioria2304@gmail.com>
2025-12-11 12:55:53 +0100

    rectif

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,6 +5,14 @@
  *
  * Return: Height of the tree, 0 if tree is NULL
  */
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t left_h = 0, right_h = 0;
@@ -12,11 +20,13 @@ size_t left_h = 0, right_h = 0;
 if (tree == NULL)
 return (0);
 
+/* Calculer la hauteur des sous-arbres */
 if (tree->left)
 left_h = 1 + binary_tree_height(tree->left);
 if (tree->right)
 right_h = 1 + binary_tree_height(tree->right);
 
+/* Retourner la plus grande hauteur */
 return (left_h > right_h ? left_h : right_h);
 }
 
