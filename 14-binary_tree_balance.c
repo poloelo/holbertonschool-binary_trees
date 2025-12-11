bd14611e4f6300c025c61223d60b25172b758102
poloelo <p.gioria2304@gmail.com>
2025-12-11 12:58:51 +0100

    rectif

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,40 +1,10 @@
 #include "binary_trees.h"
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node
- *
- * Return: Height of the tree, 0 if tree is NULL
- */
-#include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node
+ * binary_tree_balance - Measures the balance factor
+ * @tree: Pointer to root node
  *
- * Return: Height of the tree, 0 if tree is NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-size_t left_h = 0, right_h = 0;
-
-if (tree == NULL)
-return (0);
-
-/* Calculer la hauteur des sous-arbres */
-if (tree->left)
-left_h = 1 + binary_tree_height(tree->left);
-if (tree->right)
-right_h = 1 + binary_tree_height(tree->right);
-
-/* Retourner la plus grande hauteur */
-return (left_h > right_h ? left_h : right_h);
-}
-
-/**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: Balance factor (left height - right height), 0 if tree is NULL
+ * Return: Balance factor (left height - right height)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
@@ -45,7 +15,6 @@ return (0);
 
 if (tree->left)
 left_h = (int)binary_tree_height(tree->left);
-
 if (tree->right)
 right_h = (int)binary_tree_height(tree->right);
 
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,15 +4,7 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node
  *
- * Return: Height of the tree, 0 if tree is NULL
- */
-#include "binary_trees.h"
-
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node
- *
- * Return: Height of the tree, 0 if tree is NULL
+ * Return: Height of the tree, 0 if tree is NULL or leaf
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -21,7 +13,7 @@ size_t left_h = 0, right_h = 0;
 if (tree == NULL)
 return (0);
 
-/* Calculer la hauteur des sous-arbres */
+/* Calculer la hauteur des sous-arbres gauche et droit */
 if (tree->left)
 left_h = 1 + binary_tree_height(tree->left);
 if (tree->right)
